60642defd7dec8654f41e5db923e255ebeeb52b1
XuanLoc <chuxuanloc2003@gmail.com>
2024-12-24 13:46:46 +0700

    update api-gateway, eureka

--- api-gateway/src/main/java/dev/cxl/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/dev/cxl/api_gateway/ApiGatewayApplication.java
@@ -1,14 +1,15 @@
-package dev.cxl.Storage.Service;
+package dev.cxl.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class StorageServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(StorageServiceApplication.class, args);
-    }
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
 }
--- iam-service/src/main/java/dev/cxl/iam_service/IAMServiceApplication.java
+++ iam-service/src/main/java/dev/cxl/iam_service/IAMServiceApplication.java
@@ -2,11 +2,13 @@ package dev.cxl.iam_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 // (scanBasePackages = {"dev.cxl.iam_service", "com.evo.common"})
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class IAMServiceApplication {
 
     public static void main(String[] args) {
